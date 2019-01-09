5b54dbe381cc246da4471f8df59b7032cba081cd
Kostadin Terziev <kostadinterziev@Kostadins-MacBook-Pro.local>
2019-01-09 18:22:06 +0200

    splitting sidebar into component

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,36 +1,6 @@
 <template>
   <div class="tl-wrapper">
-    <nav class="tl-nav">
-      <nuxt-link to="/">
-        <img class="logo" src="Logo.svg" alt="logo-img">
-      </nuxt-link>
-      <h1>Todor Licheff</h1>
-      <h5>UI / UX Designer</h5>
-      <p class="descriptionCopy">A young multidisciplinary artist with a solid background in User Experience/Interfaces, Branding & WEB Design. The dynamics of progress and new challenges developed a keen interest in the field of brand perceptions. A self-driven hard worker and a passionate freethinker who is always looking forward to expand their worldview.</p>
-      <a @click="resumeView = !resumeView" class="resumeBtn">View Resume &#8594;</a>
-      <div class="flex socials">
-        <div>
-          <a href="https://www.behance.net/Licheff" target="_blank">
-            <img src="/Behance.svg" alt="behance-img">
-          </a>
-        </div>
-        <div>
-          <a href="https://www.linkedin.com/in/licheff" target="_blank">
-            <img src="/Linkedin.svg" alt="linkedin-img">
-          </a>
-        </div>
-        <div>
-          <a href="https://dribbble.com/Licheff" target="_blank">
-            <img src="/Dribbble.svg" alt="dribble-img">
-          </a>
-        </div>
-        <div>
-          <a href="https://www.instagram.com/licheff.dz/" target="_blank">
-            <img src="/Instagram.svg" alt="instagram-img">
-          </a>
-        </div>
-      </div>
-    </nav>
+    <side-nav></side-nav>
     <nuxt/>
     <transition name="slide">
       <div class="resume" v-show="resumeView">
@@ -47,7 +17,11 @@
   </div>
 </template>
 <script>
+import sideNav from '~/components/SideNav';
 export default {
+  components:{
+    sideNav
+  },
   data(){
     return{
       resumeView:false,
